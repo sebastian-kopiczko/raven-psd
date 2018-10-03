d0fcf10ec3d054c780fba36af4f639ec066342fd
Sebastian Kopiczko <sebastian.kopiczko@gmail.com>
2018-10-03 15:08:25 +0200

    JSX changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -19,27 +19,34 @@ export default ({ data }) => (
       }
     `}
     render={data => (
-      <header className="header">
-        <Link to="/" className="header__logo">
-          <img src={logo} alt="" />
-          <span className="header__title">
-            {data.site.siteMetadata.siteTitle}
-          </span>
-        </Link>
-        <nav className="navigation">
-          <ul className="navigation__list">
-            {data.site.siteMetadata.siteNav.map((li, index) => {
-              return (
-                <li key={index} className="navigation__item">
-                  <Link to={li.slug} className="navigation__link">
-                    {li.name}
-                  </Link>
-                </li>
-              )
-            })}
-          </ul>
-        </nav>
-      </header>
+      <div className="container">
+        <header className="header">
+          <div className="header__logo">
+            <Link className="header__link" to="/">
+              <img src={logo} alt="" />
+              <span className="header__title text-uppercase">
+                {data.site.siteMetadata.siteTitle}
+              </span>
+            </Link>
+          </div>
+          <div className="header__nav">
+            <button id="menu-btn">&#9776;</button>
+            <nav className="navigation">
+              <ul className="navigation__list">
+                {data.site.siteMetadata.siteNav.map((li, index) => {
+                  return (
+                    <li key={index} className="navigation__item">
+                      <Link to={li.slug} className="navigation__link">
+                        {li.name}
+                      </Link>
+                    </li>
+                  )
+                })}
+              </ul>
+            </nav>
+          </div>
+        </header>
+      </div>
     )}
   />
 )
