f38c577458897e7fd6db7883de38ba36bfc03749
Sebastian Kopiczko <sebastian.kopiczko@gmail.com>
2018-10-04 13:55:57 +0200

    Content modify

--- data/content.js
+++ data/content.js
@@ -6,4 +6,39 @@ module.exports = {
   footer: {
     copyright: 'Copyright © 2018',
   },
+
+  services: [
+    {
+      content: 'Richard McClint',
+      icon: 'pulse',
+    },
+    {
+      content: 'Aldus PageMaker',
+      icon: 'compass',
+    },
+    {
+      content: 'setting industry',
+      icon: 'shield',
+    },
+    {
+      content: 'building service',
+      icon: 'truck',
+    },
+    {
+      content: 'letraset sheets',
+      icon: 'vehicle',
+    },
+    {
+      content: 'Bonorum',
+      icon: 'box',
+    },
+    {
+      content: 'consectetur',
+      icon: 'cd',
+    },
+    {
+      content: 'Hampen Sydney',
+      icon: 'syringe',
+    },
+  ],
 }
