49425af3ec189be8d111457dc6f1eea9f087518a
Sebastian Kopiczko <sebastian.kopiczko@gmail.com>
2018-10-08 17:02:39 +0200

    Add projects and testimonials

--- data/content.js
+++ data/content.js
@@ -41,4 +41,71 @@ module.exports = {
       icon: 'syringe',
     },
   ],
+  projects: [
+    {
+      thumbnail: 'thumb_1',
+      original: 'origin_1',
+    },
+    {
+      thumbnail: 'thumb_2',
+      original: 'origin_2',
+    },
+    {
+      thumbnail: 'thumb_3',
+      original: 'origin_3',
+    },
+    {
+      thumbnail: 'thumb_4',
+      original: 'origin_4',
+    },
+    {
+      thumbnail: 'thumb_5',
+      original: 'origin_5',
+    },
+    {
+      thumbnail: 'thumb_6',
+      original: 'origin_6',
+    },
+    {
+      thumbnail: 'thumb_7',
+      original: 'origin_7',
+    },
+    {
+      thumbnail: 'thumb_8',
+      original: 'origin_8',
+    },
+    {
+      thumbnail: 'thumb_9',
+      original: 'origin_9',
+    },
+    {
+      thumbnail: 'thumb_10',
+      original: 'origin_10',
+    },
+    {
+      thumbnail: 'thumb_11',
+      original: 'origin_11',
+    },
+    {
+      thumbnail: 'thumb_12',
+      original: 'origin_12',
+    },
+  ],
+  testimonials: [
+    {
+      name: 'heineken',
+      content:
+        'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters.',
+    },
+    {
+      name: 'example-company',
+      content:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet debitis asperiores ipsum ea magni illo necessitatibus in provident.',
+    },
+    {
+      name: 'example-company2',
+      content:
+        'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters.Eveniet debitis asperiores ipsum ea magni illo necessitatibus in provident.',
+    },
+  ],
 }
