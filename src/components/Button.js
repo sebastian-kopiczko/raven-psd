b5ff3f05a5e4634d7049926418f0eec5c8ded4c4
Sebastian Kopiczko <sebastian.kopiczko@gmail.com>
2018-10-08 17:08:11 +0200

    Work on button component

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,5 +1,12 @@
 import React from 'react'
 
 export default props => (
-  <button className="button btn">{props.buttonText}</button>
+  <button
+    className={`button button--${props.color} ${
+      props.centered ? 'center' : ''
+    }`}
+  >
+    {props.buttonText}
+  </button>
 )
+// ${this.props.showBulkActions ? 'shown' : 'hidden'}
