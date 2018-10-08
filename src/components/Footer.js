187f95b8f6dca08ea89bbcc4a6121c45ebeb9efa
Sebastian Kopiczko <sebastian.kopiczko@gmail.com>
2018-10-08 17:12:11 +0200

    Footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -20,23 +20,29 @@ export default () => (
     `}
     render={data => (
       <footer className="footer">
-        <p className="footer__copyright">
-          {data.site.siteMetadata.footer.copyright}
-        </p>
-        <ul className="social-links navigation__list">
-          {data.site.siteMetadata.socialNav.map((li, index) => {
-            return (
-              <li key={index} className="navigation__item">
-                <Link to={li.url} className="navigation__link">
-                  <i className={li.icon} />
-                </Link>
+        <div className="container">
+          <div className="row">
+            <p className="footer__copyright">
+              {data.site.siteMetadata.footer.copyright}
+            </p>
+            <ul className="footer__navigation">
+              {data.site.siteMetadata.socialNav.map((li, index) => {
+                return (
+                  <li
+                    key={index}
+                    className={`footer__navigation-item icon--${li.icon}`}
+                  >
+                    {index}
+                    <Link to={li.url} className="footer__navigation-link" />
+                  </li>
+                )
+              })}
+              <li className="navigation__item scroll-up">
+                <i className="fa fa-arrow-up" />
               </li>
-            )
-          })}
-          <li className="navigation__item scroll-up">
-            <i className="fa fa-arrow-up" />
-          </li>
-        </ul>
+            </ul>
+          </div>
+        </div>
       </footer>
     )}
   />
